8f5a34ce3ee28264686686bd7489ea1d233245e0
angganu <annu.nura@gmail.com>
2026-07-22 19:24:16 +0700

    remove modal

--- resources/views/task-categories/index.blade.php
+++ resources/views/task-categories/index.blade.php
@@ -126,31 +126,11 @@
                     <hr>
                     <div class="category-card__actions">
                         <a href="{{ route('task-categories.edit', $category) }}" class="btn btn-sm btn-outline-light">Edit</a>
-                        <button type="button" class="btn btn-sm btn-outline-danger" data-bs-toggle="modal" data-bs-target="#deleteCategoryModal-{{ $category->id }}">
-                            Delete
-                        </button>
-                    </div>
-
-                    <div class="modal fade" id="deleteCategoryModal-{{ $category->id }}" tabindex="-1" aria-labelledby="deleteCategoryModalLabel-{{ $category->id }}" aria-hidden="true" data-bs-backdrop="false">
-                        <div class="modal-dialog modal-dialog-centered">
-                            <div class="modal-content bg-dark text-white">
-                                <div class="modal-header border-secondary">
-                                    <h5 class="modal-title" id="deleteCategoryModalLabel-{{ $category->id }}">Delete Category</h5>
-                                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <p class="mb-0">Are you sure you want to delete <strong>{{ $category->name }}</strong>?</p>
-                                </div>
-                                <div class="modal-footer border-secondary">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                                    <form action="{{ route('task-categories.destroy', $category) }}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-danger">Delete</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
+                        <form action="{{ route('task-categories.destroy', $category) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete {{ addslashes($category->name) }}?')">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
+                        </form>
                     </div>
                 </div>
             @empty
